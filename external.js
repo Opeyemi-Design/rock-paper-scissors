65ae8377b88edde86feade4d428e2fccd086280b
Opeyemi Adetifa <opeyemiadetifa17@gmail.com>
2023-06-04 17:43:21 +0100

    Create a 5 round rock paper scissor game

--- external.js
+++ external.js
@@ -8,7 +8,7 @@ const getComputerChoice = () => {
     const caseInsensitivePlayerSelection = playerSelection.toLowerCase()
 
     if (caseInsensitivePlayerSelection === computerSelection.toLowerCase()) {
-        return "Wow! It's a stalemate"
+        return "Stalemate"
     } else if (    (caseInsensitivePlayerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
     (caseInsensitivePlayerSelection === 'paper' && computerSelection.toLowerCase() === 'rock') ||
     (caseInsensitivePlayerSelection === 'scissors' && computerSelection.toLowerCase() === 'paper')) {
@@ -18,6 +18,37 @@ const getComputerChoice = () => {
     }
   }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(gameRound(playerSelection, computerSelection));
+  const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+      const computerSelection = getComputerChoice();
+  
+      console.log(`Round ${round}:`);
+      console.log(`Player chooses ${playerSelection}`);
+      console.log(`Computer chooses ${computerSelection}`);
+  
+      const result = gameRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes("win")) {
+        playerScore++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+      }
+    }
+  
+    console.log("Game's finished!");
+  
+    if (playerScore > computerScore) {
+      console.log(`Congratulations! You win the game with a score of ${playerScore}:${computerScore}`);
+    } else if (playerScore < computerScore) {
+      console.log(`Oops! You lose the game with a score of ${playerScore}:${computerScore}`);
+    } else {
+      console.log(`It's a tie! The game ends with a score of ${playerScore}:${computerScore}`);
+    }
+  }
+  
+  game();
