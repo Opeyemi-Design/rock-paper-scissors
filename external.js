df712b1c9f2c679f3c072802084e8ec9ea4c078e
Opeyemi Adetifa <opeyemiadetifa17@gmail.com>
2023-06-04 16:31:23 +0100

    Generate random computer choice

---
+++ external.js
@@ -0,0 +1,5 @@
+const getComputer = () => {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  };
