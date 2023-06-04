4256160a3e487866b887455bfe7f04626833c468
Opeyemi Adetifa <opeyemiadetifa17@gmail.com>
2023-06-04 17:29:57 +0100

    Play one round of rock paper scissors

--- external.js
+++ external.js
@@ -1,5 +1,23 @@
-const getComputer = () => {
+const getComputerChoice = () => {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   };
+
+  const gameRound = (playerSelection, computerSelection) => {
+    const caseInsensitivePlayerSelection = playerSelection.toLowerCase()
+
+    if (caseInsensitivePlayerSelection === computerSelection.toLowerCase()) {
+        return "Wow! It's a stalemate"
+    } else if (    (caseInsensitivePlayerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
+    (caseInsensitivePlayerSelection === 'paper' && computerSelection.toLowerCase() === 'rock') ||
+    (caseInsensitivePlayerSelection === 'scissors' && computerSelection.toLowerCase() === 'paper')) {
+        return `You win! ${caseInsensitivePlayerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${caseInsensitivePlayerSelection}`;
+    }
+  }
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(gameRound(playerSelection, computerSelection));
